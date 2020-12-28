662ed33b922c36c3bac049336e6e133ac72be145
Céline Barbe <laloutrecosmique@hotmail.fr>
2020-12-28 11:59:26 +0100

    sync method

--- server.js
+++ server.js
@@ -12,6 +12,29 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = require('./app/models');
+const Role = db.role;
+
+db.sequelize.sync({force: true}).then(() => {
+    console.log('Drop and Resync DB');
+    initial();
+});
+
+function initial() {
+    Role.create({
+        id: 1,
+        name: 'user'
+    });
+    Role.create({
+        id: 2,
+        name: 'moderator'
+    });
+    Role.create({
+        id: 3,
+        name: 'admin'
+    });
+};
+
 //route
 app.get('/', (req, res) => {
     res.json({message: `Bienvenue sur mon application`});
